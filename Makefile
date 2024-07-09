6c68298a2cf0600b01bdbdb1d956c630517aebd7
Chawit Chaijirawiwat <chawitcgithub@gmail.com>
2024-07-09 23:50:41 +0900

    touch file and dummy app for project structure

--- Makefile
+++ Makefile
@@ -3,13 +3,18 @@ install:
 		pip install -r requirements.txt
 
 test:
-	python -m pytest -vv test_hello.py
+	python -m pytest -vv --cov=cli --cov=mlib --cov=utilscli --cov=app test_mlib.py
 
 format:
 	black *.py
 
-
 lint:
-	pylint --disable=R,C hello.py
+	pylint --disable=R,C,W1203,E1101 *.py # mlib cli utilscli
+	# lint Dockerfile
+	# docker run --rm -i hadolint/hadolint < Dockerfile
 
-all: install lint test
+# deploy:
+	# push to ECR for deploy
+	# tbd
+	
+all: install lint test format # deploy
---
+++ app.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "hello root route"}
+
+
+@app.get("/add/{num1}/{num2}")
+async def add(num1: int, num2: int):
+    total = num1 + num2
+    return {"result": total}
+
+
+@app.get("/subtract/{num1}/{num2}")
+async def subtract(num1: int, num2: int):
+    total = num1 - num2
+    return {"result": total}
+
+
+@app.get("/multiply/{num1}/{num2}")
+async def multiply(num1: int, num2: int):
+    total = num1 * num2
+    return {"result": total}
+
+
+@app.get("/divide/{num1}/{num2}")
+async def divide(num1: int, num2: int):
+    total = num1 / num2
+    return {"result": total}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0")
